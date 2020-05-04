9165f46b69c536ba7c6a38365f0f69180bb82816
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-05-03 19:30:44 -0700

    assn 6 meeting and detail
    creates view to return all meetings
    creates view to return meeting details by meeting id
    creates meetings.html to list meetings
    creates meetingdetails.html to display meeting details
    adds meeting link to base temp
    adds meetingdetails to path with id ref
    links meetingdetails to individual meetings

--- club/views.py
+++ club/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +9,18 @@ def index (request):
 def getResources(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resources.html', {'resource_list' : resource_list})
+
+def getMeetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_list' : meeting_list})
+
+def meetingDetails(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)  # don't forget the model name here!!
+    # discount=prod.memberdiscount
+    # reviews=Review.objects.filter(product=id).count()
+    context={
+        'meeting' : meeting,
+        # 'discount' : discount,
+        # 'reviews' : reviews,
+    }
+    return render(request, 'club/meetingdetails.html', context=context)
