6e99981fa83e93892215cbf77ac040f2aa62dcca
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-05-31 21:37:53 -0700

    assn 8 tests
    creates forms.py
    adds forms for resources and meetings
    adds views for resource and meeting forms
    adds newresource and newmeeting subpages
    adds new subpages to urls
    minor edits of all html pages to make quotes consistent
    fixes nav to have page links in the correct list layout

--- club/urls.py
+++ club/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     path('resources/', views.getResources, name='resources'),
     path('meetings/', views.getMeetings, name='meetings'),
     path('meetingdetails/<int:id>', views.meetingDetails, name='meetingdetails'),
+    path('newresource/', views.newResource, name='newresource'),
+    path('newmeeting/', views.newMeeting, name='newmeeting'),    
 ]
--- club/views.py
+++ club/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
+from .forms import MeetingForm, ResourceForm
 
 # Create your views here.
 def index (request):
@@ -24,3 +25,29 @@ def meetingDetails(request, id):
         # 'reviews' : reviews,
     }
     return render(request, 'club/meetingdetails.html', context=context)
+
+
+def newResource(request):
+    form = ResourceForm
+    if request.method == 'POST':
+        form = ResourceForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = True)
+            post.save()
+            form = ResourceForm()
+    else:
+        form = ResourceForm()
+    return render(request, 'club/newresource.html', {'form': form})
+
+
+def newMeeting(request):
+    form = MeetingForm
+    if request.method == 'POST':
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = True)
+            post.save()
+            form = MeetingForm()
+    else:
+        form = MeetingForm()
+    return render(request, 'club/newmeeting.html', {'form': form})
